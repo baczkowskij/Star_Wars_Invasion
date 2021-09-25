81af34d9419859109d1ed8954ab5cfebaa9a655f
baczkowskij <90989648+baczkowskij@users.noreply.github.com>
2021-09-25 16:40:11 +0200

    Moving Ship Defined

--- ship.py
+++ ship.py
@@ -6,6 +6,19 @@ class Ship:
         self.image = pygame.image.load("data/ship.bmp")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right=False
+        self.moving_left=False
+        self.settings = ai_game.settings
+        self.x = self.rect.x
     def blitme(self):
         '''Draw a ship in a current location:'''
         self.screen.blit(self.image,self.rect)
+
+    def update(self):
+        if self.moving_right and self.rect.right<self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.x>0:
+            self.x -= self.settings.ship_speed
+        else:
+            self.x=self.x
+        self.rect.x=self.x
